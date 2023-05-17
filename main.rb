b089ac0cbec1f979b0c4b5f96ee49798a8408743
safarali <rafilsafargaliev@gmail.com>
2023-05-17 17:48:35 +0300

    first commit

---
+++ main.rb
@@ -0,0 +1,16 @@
+if Gem.win_platform?
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
+
+require_relative 'lib/product'
+require_relative 'lib/book'
+require_relative 'lib/film'
+
+leon = Film.new(price: 990, amount: 5)
+
+puts "Фильм Леон стоит #{leon.price} руб."
